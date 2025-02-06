f99b20a94e4143c0b0c72b7f2490e65dd2e5c175
andikalp313 <andikaluhurpambudi@gmail.com>
2025-02-06 19:03:54 +0700

    feat: implement full CRUD for room non availability (#24)
    * chore:user-profile
    
    * feat: implement full CRUD for room management
    
    * feat: implement full CRUD for room non availability

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import xenditRouter from "./routes/xendit.router";
 import reviewRouter from "./routes/review.router";
 import categoryRouter from "./routes/category.router";
 import roomRouter from "./routes/room.router";
+import roomNonAvailabilityRouter from "./routes/roomNonAvailability.router";
 // import { initializeAutoCheckInOut } from "./services/transaction/autoCheckInOut.Service";
 
 const app = express();
@@ -26,6 +27,7 @@ app.use("/xendit", xenditRouter);
 app.use("/reviews", reviewRouter);
 app.use("/categories", categoryRouter);
 app.use("/rooms", roomRouter);
+app.use("/room-non-availabilities", roomNonAvailabilityRouter);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(400).send(err.message);
