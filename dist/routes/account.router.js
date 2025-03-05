3b7ad1701b217ac08c51efeb3f09644ccec0fd4e
andikalp313 <andikaluhurpambudi@gmail.com>
2025-03-05 23:33:22 +0700

    fix:User Profile and tenant profile

--- dist/routes/account.router.js
+++ dist/routes/account.router.js
@@ -8,10 +8,11 @@ const profilePictureFilter_1 = require("../lib/profilePictureFilter");
 const router = (0, express_1.Router)();
 router.get("/profile", jwt_1.verifyToken, account_controller_1.getProfileController);
 router.get("/tenant", jwt_1.verifyToken, account_controller_1.getTenantController);
-router.patch("/", jwt_1.verifyToken, (0, multer_1.uploader)(1).fields([{ name: "imageFile", maxCount: 1 }]), profilePictureFilter_1.fileFilterProfile, account_controller_1.updateProfileController);
+// In account.router.ts for the user profile endpoint
+router.patch("/", jwt_1.verifyToken, (0, multer_1.uploader)(1).single("imageFile"), profilePictureFilter_1.fileFilterProfile, account_controller_1.updateProfileController);
 router.patch("/change-password", jwt_1.verifyToken, account_controller_1.changePasswordController);
 router.post("/verify-change-email", account_controller_1.verifyChangeEmailController);
 router.post("/change-email", jwt_1.verifyToken, account_controller_1.changeEmailController);
-// Move parameter route last
+// In account.router.ts
 router.patch("/tenant", jwt_1.verifyToken, (0, multer_1.uploader)(1).single("imageFile"), profilePictureFilter_1.fileFilterProfile, account_controller_1.updateTenantProfileController);
 exports.default = router;
